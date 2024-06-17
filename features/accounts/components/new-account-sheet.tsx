c1eda59b84696388a858faf29c5e9a5964f2bbf0
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-06-18 00:40:52 +0100

    implemented creation of name for account

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -6,9 +6,30 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useNewAccount } from "../hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+import { useCreateAccount } from "../api/use-create-account";
+
+const formSchema = insertAccountSchema.pick({
+    name: true
+})
+
+
+type FormValues = z.input<typeof formSchema>;
+
 
 export const NewAccountSheet = () => {
   const {isOpen, onClose} = useNewAccount();
+  const mutation = useCreateAccount();
+  const onSubmit = (values: FormValues) => {
+     mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      }
+     });
+  };
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
@@ -16,6 +37,13 @@ export const NewAccountSheet = () => {
           <SheetTitle>New Account</SheetTitle>
           <SheetDescription>Create a new account</SheetDescription>
         </SheetHeader>
+        <AccountForm 
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValue={{
+            name: '',
+          }}
+        />
       </SheetContent>
     </Sheet>
   );
