5185ff1b3c5ae64231d4c3fc5417f755a50be259
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-07-16 15:44:07 +0100

    implemented summary for transaction in the backend

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,10 @@ export function formatCurrency(amount: number) {
     currency: 'USD',
   }).format(amount);
 }
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0){
+    return previous === current ? 0 : 100
+  }
+  return Math.round(((current - previous) / previous) * 100);
+}
