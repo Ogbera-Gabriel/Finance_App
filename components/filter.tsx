6fa3041ae1659c58d49aa1198029d15ac9c6c257
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-08-02 21:16:48 +0100

    "implemented date-filter"

--- components/filter.tsx
+++ components/filter.tsx
@@ -7,7 +7,7 @@ export const Filters = () => {
          className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2"
         >
           <AccountFilter />
-          {/* <DateFilter /> */}
+          <DateFilter />
         </div>
     )
 }
