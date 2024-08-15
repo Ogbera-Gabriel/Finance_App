8d01488e887b982e7033f169080bdcb1a4458515
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-08-15 17:53:36 +0100

    fixed bug with categories fetching

--- features/categories/api/use-get-categories.ts
+++ features/categories/api/use-get-categories.ts
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
+import { useAuth } from "@clerk/nextjs";
 
 
 export const useGetCategories = () => {
+    const {isLoaded, userId} = useAuth();
     const query = useQuery({
         queryKey: ["categories"],
         queryFn: async () => {
+            if (!userId) {
+                throw new Error("User not authenticated");
+            }
             const response = await client.api.categories.$get();
 
             if (!response.ok) {
@@ -14,7 +19,8 @@ export const useGetCategories = () => {
 
             const { data } = await response.json();
             return data;
-        }
+        },
+        enabled: isLoaded && !!userId
     });
 
     return query;
