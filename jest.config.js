5ff314ebb9bdf7ff95281825d90807b2db45ce9f
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-08-16 16:25:47 +0100

    react test libary is bugged

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Path to your jest.setup.js file
   testEnvironment: 'jsdom', // Use jsdom to simulate a browser-like environment
   // Other custom configurations can go here
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 }
 
 // Export the Jest configuration
